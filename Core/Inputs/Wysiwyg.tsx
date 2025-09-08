1e99c79452abe15a2b96116d14af23c214f34425
Dusan <dusan.daniska@wai.sk>
2025-09-08 21:20:33 +0200

    textarea with raw html in wysiwyg

--- Core/Inputs/Wysiwyg.tsx
+++ Core/Inputs/Wysiwyg.tsx
@@ -4,6 +4,7 @@ import * as uuid from 'uuid';
 import { Editor } from 'primereact/editor';
 
 interface WysiwygInputState extends InputState {
+  textareaValue: string,
 }
 
 export default class Wysiwyg extends Input<InputProps, WysiwygInputState> {
@@ -12,23 +13,51 @@ export default class Wysiwyg extends Input<InputProps, WysiwygInputState> {
     id: uuid.v4(),
   }
 
+  refTextarea: any;
+
   constructor(props: InputProps) {
     super(props);
+
+    this.refTextarea = React.createRef();
 
     this.state = {
       ...this.state, // Parent state
       isInitialized: true,
+      textareaValue: this.state.value,
     };
   }
 
   renderInputElement() {
-    return <>
-      <Editor
-        className="w-full relative"
-        style={{ height: '320px' }}
-        value={this.state.value}
-        onTextChange={(e) => this.onChange(e.htmlValue)} 
-      />
-    </>;
+    return <div className='flex gap-2'>
+      <div className='flex-1'>
+        <Editor
+          className="w-full relative"
+          style={{ height: '320px' }}
+          value={this.state.value}
+          onTextChange={(e) => {
+            this.onChange(e.htmlValue);
+            this.setState({textareaValue: e.htmlValue});
+          }}
+        />
+      </div>
+      <div className='flex-1'>
+        <textarea
+          className='w-full'
+          value={this.state.textareaValue}
+          onChange={(e) => {
+            this.setState({textareaValue: e.target.value});
+          }}
+        />
+        <button
+          className='btn btn-transparent mt-2'
+          onClick={() => {
+            this.onChange(this.state.textareaValue);
+          }}
+        >
+          <span className='icon'><i className='fas fa-arrow-left'></i></span>
+          <span className='text'>Use this HTML code</span>
+        </button>
+      </div>
+    </div>;
   }
 }
