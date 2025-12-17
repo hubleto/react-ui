9586310638d6a85eba841bc50c2111c7c1f496aa
Dusan <dusan.daniska@wai.sk>
2025-12-17 19:03:56 +0100

    added HtmlFrame component

---
+++ core/HtmlFrame.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { HubletoComponentProps } from './Component';
+
+export interface HtmlFrameProps {
+  content?: string,
+  className?: string,
+}
+
+export interface HtmlFrameState {
+  content?: string,
+}
+
+export default class HtmlFrame extends Component<HtmlFrameProps, HtmlFrameState> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { content: this.props.content }
+  }
+
+  render() {
+    return <>
+      <iframe
+        src="about:blank"
+        className={this.props.className}
+        srcDoc={this.state.content}
+      />
+    </>
+  }
+}
